3e95d27825df68b95bd1ec8893c0d944e4d9d256
Tom <tom.divers9@gmail.com>
2022-05-07 14:03:18 +0100

    why

--- app/ReadGrammar.hs
+++ app/ReadGrammar.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module ReadGrammar where
 
@@ -8,11 +9,26 @@ import Turtlehead
 import Data.Aeson
 import Data.Text
 import GHC.Generics
+import System.IO
+import qualified Data.ByteString.Lazy as B
 
 data ParsedGrammar = ParsedGrammar {
     rules :: [(Text, Text)],
-    imap :: [(Text, Float)],
+    imap :: [(Text, [(Text, Float)])],
     startString :: Text,
     iters :: Int
-}
+} deriving (Generic, Show)
+
+instance FromJSON ParsedGrammar where
+    parseJSON (Object v) = ParsedGrammar 
+        <$> v .: "rules" 
+        <*> v .: "imap"
+        <*> v .: "startString"
+        <*> v .: "iters"
+
+
+readGrammar :: FilePath -> IO (Maybe ParsedGrammar)
+readGrammar path = do
+    byteString <- B.readFile path
+    return $ decode byteString
 
